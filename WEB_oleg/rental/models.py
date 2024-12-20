259fd7574cf9682012f5f72bc1c1eac3e989941d
Iversy <iversy49@ya.ru>
2024-12-21 00:19:19 +0500

    datetime default value, comboboxes values fixed

--- WEB_oleg/rental/models.py
+++ WEB_oleg/rental/models.py
@@ -5,17 +5,23 @@ class Client(models.Model):
     license = models.CharField(max_length=255)
     birthday = models.DateField()
     phone = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
     year = models.IntegerField()
     color = models.CharField(max_length=50)
     number = models.CharField(max_length=20)
+    def __str__(self):
+        return self.model
 
 class Service(models.Model):
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     payment_details = models.TextField()
+    def __str__(self):
+        return self.name
 
 class Rent(models.Model):
     client = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True)  
