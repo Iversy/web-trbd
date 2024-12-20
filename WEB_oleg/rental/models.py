b904b2da26cf0169074aa0fc8f7b80b121010299
Iversy <iversy49@ya.ru>
2024-12-21 01:11:24 +0500

    мышиная валидация

--- WEB_oleg/rental/models.py
+++ WEB_oleg/rental/models.py
@@ -1,4 +1,20 @@
+import re
 from django.db import models
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+
+
+def validate_year(value):
+    if value < 1672:
+        raise ValidationError('В 1672 году Фердинанд Вербист придумал первую миниатюрную модель самодвижущейся повозки с использованием парового котла. Это изобретение было создано как игрушка для китайского императора и не было предназначено для перевозки водителя или пассажира, но, вероятно, оно является первой самодвижущейся паровой машиной. А вы указали год до этого...')
+    if value > timezone.now().year:
+        raise ValidationError('Год не может быть позже текущего.')
+
+def validate_number_plate(value):
+    pattern = r'^[АВЕКМНОРСТУХ]{1} \d{3} [АВЕКМНОРСТУХ]{2} \d{2,3}$'
+    if not re.match(pattern, value):
+        raise ValidationError('Неправильный формат номера. Формат: "A 123 AB 12" или "A 123 AB 123".')
+
 
 class Client(models.Model):
     name = models.CharField(max_length=255)
@@ -10,9 +26,9 @@ class Client(models.Model):
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
-    year = models.IntegerField()
+    year = models.IntegerField(validators=[validate_year])
     color = models.CharField(max_length=50)
-    number = models.CharField(max_length=20)
+    number = models.CharField(max_length=20, validators=[validate_number_plate])
     def __str__(self):
         return self.model
 
@@ -30,9 +46,23 @@ class Rent(models.Model):
     end = models.DateTimeField()
     price = models.FloatField()
 
+    def clean(self):
+        super().clean()
+        if self.start > self.end:
+            raise ValidationError('Дата начала аренды должна быть меньше или равна дате окончания.')
+        if self.start > timezone.now() or self.end > timezone.now():
+            raise ValidationError('Даты начала и окончания аренды не должны быть в будущем.')
+
 class Maintenance(models.Model):
     car = models.ForeignKey(Car, on_delete=models.SET_NULL, null=True)          
     service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True)  
     start = models.DateTimeField()
     end = models.DateTimeField()
     price = models.FloatField()
+
+    def clean(self):
+        super().clean()
+        if self.start > self.end:
+            raise ValidationError('Дата начала обслуживания должна быть меньше или равна дате окончания.')
+        if self.start > timezone.now() or self.end > timezone.now():
+            raise ValidationError('Даты начала и окончания обслуживания не должны быть в будущем.')
